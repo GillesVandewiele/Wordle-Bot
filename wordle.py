a19d2d5ff9a6b36b52aa851ab460c815a92c89bc
Dawghen <mechanalyses@gmail.com>
2022-02-17 15:48:57 -0800

    add all_words and other update

--- wordle.py
+++ wordle.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import os
 import itertools
 import random
@@ -6,10 +7,10 @@ from tqdm import tqdm
 from scipy.stats import entropy
 from collections import defaultdict, Counter
 
-N_GAMES = 10
-N_GUESSES = 6
+N_GUESSES = 10
+DICT_FILE_all = 'all_words.txt'
 DICT_FILE = 'words.txt'
-
+SAVE_TIME = False
 
 def calculate_pattern(guess, true):
     """Generate a pattern list that Wordle would return if you guessed
@@ -66,53 +67,68 @@ def calculate_entropies(words, possible_words, pattern_dict):
     return entropies
 
 
-# Load our dictionary
-with open(DICT_FILE) as ifp:
-    dictionary = list(map(lambda x: x.strip(), ifp.readlines()))
-error_msg = 'Dictionary contains different length words.'
-assert len({len(x) for x in dictionary}) == 1, error_msg
-print(f'Loaded dictionary with {len(dictionary)} words...')
-WORD_LEN = len(dictionary[0])
+def main():
+    # load all 5-letter-words for making patterns 
+    with open(DICT_FILE_all) as ifp:
+        all_dictionary = list(map(lambda x: x.strip(), ifp.readlines()))
 
-# Generate the possible patterns of information we can get
-all_patterns = list(itertools.product([0, 1, 2], repeat=WORD_LEN))
+    # Load 2315 words for solutions
+    with open(DICT_FILE) as ifp:
+        dictionary = list(map(lambda x: x.strip(), ifp.readlines()))
 
-# Calculate the pattern_dict and cache it, or load the cache.
-if 'pattern_dict.p' in os.listdir('.'):
-    pattern_dict = pickle.load(open('pattern_dict.p', 'rb'))
-else:
-    pattern_dict = generate_pattern_dict(dictionary)
-    pickle.dump(pattern_dict, open('pattern_dict.p', 'wb+'))
+    error_msg = 'Dictionary contains different length words.'
+    assert len({len(x) for x in all_dictionary}) == 1, error_msg
+    print(f'Loaded dictionary with {len(all_dictionary)} words...')
+    WORD_LEN = len(all_dictionary[0]) # 5-letters 
 
-# Simulate games
-for _ in range(N_GAMES):
+    # Generate the possible patterns of information we can get
+    all_patterns = list(itertools.product([0, 1, 2], repeat=WORD_LEN))
 
-    # Pick a random word for the bot to guess
-    WORD_TO_GUESS = random.choice(dictionary)
-    print('-'*100)
-    print('Word to guess:', WORD_TO_GUESS)
+    # Calculate the pattern_dict and cache it, or load the cache.
+    if 'pattern_dict.p' in os.listdir('.'):
+        pattern_dict = pickle.load(open('pattern_dict.p', 'rb'))
+    else:
+        pattern_dict = generate_pattern_dict(all_dictionary)
+        pickle.dump(pattern_dict, open('pattern_dict.p', 'wb+'))
 
-    # Keep a list of the remaining possible words
-    all_words = set(dictionary)
-    for n_round in range(N_GUESSES):
-        # Calculate entropies
-        if len(all_words) < 10:
-            candidates = all_words
+    # Simulate games
+    stats = defaultdict(list)
+
+    for WORD_TO_GUESS in tqdm(dictionary):
+
+        if SAVE_TIME:
+            guess_word = 'tares'
+            all_words = set(all_dictionary)
+            info = calculate_pattern(guess_word, WORD_TO_GUESS)
+            words = pattern_dict['tares'][info]
+            all_words = all_words.intersection(words)
+            init_round = 1
         else:
-            candidates = dictionary
-        entropies = calculate_entropies(candidates, all_words, pattern_dict)
+            init_round = 0
 
-        # Guess the candiate with highest entropy
-        guess_word = max(entropies.items(), key=lambda x: x[1])[0]
-        info = calculate_pattern(guess_word, WORD_TO_GUESS)
+        for n_round in range(init_round, N_GUESSES):
 
-        # Print round information
-        print('Guessing:     ', guess_word)
-        print('Info:         ', info)
-        if guess_word == WORD_TO_GUESS:
-            print(f'WIN IN {n_round + 1} GUESSES!\n\n\n')
-            break
+            candidates = all_dictionary
+            entropies = calculate_entropies(candidates, all_words, pattern_dict)
 
-        # Filter our list of remaining possible words
-        words = pattern_dict[guess_word][info]
-        all_words = all_words.intersection(words)
+            if max(entropies.values()) < 0.1:
+                candidates = all_words
+                entropies = calculate_entropies(candidates, all_words, pattern_dict)
+
+            # Guess the candiate with highest entropy
+            guess_word = max(entropies.items(), key=lambda x: x[1])[0]
+            info = calculate_pattern(guess_word, WORD_TO_GUESS)
+
+            # Print round information
+            print('Guessing:     ', guess_word)
+            print('Info:         ', info)
+            if guess_word == WORD_TO_GUESS:
+                print(f'WIN IN {n_round + 1} GUESSES!\n\n\n')
+                break
+
+            # Filter our list of remaining possible words
+            words = pattern_dict[guess_word][info]
+            all_words = all_words.intersection(words)
+
+if __name__ == "__main__":
+    main()
